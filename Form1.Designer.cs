a0030553c968b6e2610cbc6317a6463e352276c5
Fatih ışılgan <fatihisilgan@hotmail.com>
2019-11-25 00:59:45 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace color_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picStream = new System.Windows.Forms.PictureBox();
+            this.colorDATA = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picStream)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picStream
+            // 
+            this.picStream.Image = ((System.Drawing.Image)(resources.GetObject("picStream.Image")));
+            this.picStream.Location = new System.Drawing.Point(77, 12);
+            this.picStream.Name = "picStream";
+            this.picStream.Size = new System.Drawing.Size(569, 335);
+            this.picStream.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStream.TabIndex = 0;
+            this.picStream.TabStop = false;
+            // 
+            // colorDATA
+            // 
+            this.colorDATA.AutoSize = true;
+            this.colorDATA.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.colorDATA.Location = new System.Drawing.Point(275, 362);
+            this.colorDATA.Name = "colorDATA";
+            this.colorDATA.Size = new System.Drawing.Size(145, 42);
+            this.colorDATA.TabIndex = 1;
+            this.colorDATA.Text = "Nodata";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(715, 448);
+            this.Controls.Add(this.colorDATA);
+            this.Controls.Add(this.picStream);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picStream)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picStream;
+        private System.Windows.Forms.Label colorDATA;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace color_reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int m, y, k;
+        Capture _capture = new Capture();
+
+        Bitmap bitmap;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+                
+            try
+            {
+
+
+
+                _capture.Start();
+                Application.Idle += streaming;
+
+
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Source);
+                MessageBox.Show(error.Message);
+
+            }
+            m = 0;
+            y = 0;
+            k = 0;
+
+
+        }
+
+        private void streaming(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                var img = _capture.QueryFrame().ToImage<Bgr, byte>();// değişik görüntü özellikleri verilebilir
+                var bpm = img.Bitmap;
+                bitmap = new Bitmap(bpm);
+                colorREAD();
+                picStream.Image = bitmap;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Source);
+                MessageBox.Show(error.Message);
+
+            }
+
+        }
+        public void colorREAD()
+        {
+            for (int i = 0; i < picStream.Width; i++)
+            {
+                for (int j = 0; j < picStream.Height; j++)
+                {
+                    if (bitmap.GetPixel(i, j).R <= 95 && bitmap.GetPixel(i, j).G <= 95 && bitmap.GetPixel(i, j).B >= 100)//mavi şartı
+                    {
+                        m++;
+                    }
+                    if (bitmap.GetPixel(i, j).R <= 95 && bitmap.GetPixel(i, j).G >= 100 && bitmap.GetPixel(i, j).B <= 95)//yeşil şartı
+                    {
+                        y++;
+                    }
+                    if (bitmap.GetPixel(i, j).R >= 105 && bitmap.GetPixel(i, j).G <= 95 && bitmap.GetPixel(i, j).B <= 95)//kırmızı şartı
+                    {
+                        k++;
+                    }
+
+                }
+            }
+
+            if (k > m && k > y)
+            {
+                colorDATA.Text = "kırmızı";
+                k = 0;
+                y = 0;
+
+                m = 0;
+            }
+            if (y > m && y > k)
+            {
+                colorDATA.Text = "yeşil";
+                k = 0;
+                y = 0;
+
+                m = 0;
+
+            }
+            if (m > k && m > y)
+            {
+                colorDATA.Text = "mavi";
+                k = 0;
+                y = 0;
+
+                m = 0;
+            }
+        }
+    }
+}
+
